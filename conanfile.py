1258d6c98a36317e3e156a3507746139a8990860
WuMo <wumo@outlook.com>
2020-05-31 19:10:33 +0800

    release plz

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conans import CMake, ConanFile, tools
+import os
+
+class VulkanHeadersConan(ConanFile):
+    name = "vulkan-headers"
+    version = "1.2.135"
+    url = "https://github.com/KhronosGroup/Vulkan-Headers"
+    description = "Vulkan Header files and API registry"
+    
+    settings = "os", "compiler", "build_type", "arch"
+    
+    no_copy_source = True
+    
+    def source(self):
+        tools.get(
+            f"https://github.com/KhronosGroup/Vulkan-Headers/archive/v{self.version}.tar.gz")
+    
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_dir=os.path.join(
+            self.source_folder, f"Vulkan-Headers-{self.version}"))
+        cmake.build()
+    
+    def package(self):
+        cmake = CMake(self)
+        cmake.install(build_dir=self.build_folder)
+    
+    def package_info(self):
+        self.user_info.VULKAN_REGISTRY_PATH = os.path.join(
+            self.package_folder, "share", "vulkan", "registry")
